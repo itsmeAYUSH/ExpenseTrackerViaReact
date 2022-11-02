9d726e10296e9d1d630959062180a7e454ddd675
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-02 22:23:44 +0530

    Update ExpenseItem.js

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,13 +1,20 @@
-function ExpenseItem(){
-    return (
-        <div>
-            <h2>Expense Item</h2>
-            <ol>
-                <li>Food rs 10</li>
-                <li>Petrol rs 100</li>
-                <li>Movies rs 200</li>
-            </ol>
-        </div>
-    )
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+    const  expenseDate = new Date(2022 , 11 , 2);
+    const expenseTitle = 'Food';
+    const expenseAmount = 10;
+    const locationOfExpense = 'Mumbai';
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__description">{locationOfExpense}</div>
+        <div className="expense-item__price">rs {expenseAmount}</div>
+        
+      </div>
+    </div>
+  ); 
 }
 export default ExpenseItem;
