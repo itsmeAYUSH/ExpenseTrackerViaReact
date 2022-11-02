f161a80777fb43fd9e9471a842dfc30b69b36d20
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-02 19:49:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem.js";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h2>Expense Item</h2>
+            <ol>
+                <li>Food rs 10</li>
+                <li>Petrol rs 100</li>
+                <li>Movies rs 200</li>
+            </ol>
+        </div>
+    )
+}
+export default ExpenseItem;
