aa2806c4de331feecb59036ebb05fd75de1deae4
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-03 14:32:26 +0530

    Update App.js
    added map instead of repeating component again and again

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
-  return (
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Mumbai",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Delhi",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "bangalore",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Kolkata",
+    },
+  ];
+  return(
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}>
+        </ExpenseItem>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
+
