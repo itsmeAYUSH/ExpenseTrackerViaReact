f0a013c1c5bfe8f6b94098c0d3f1ae8196fc3937
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-03 22:16:55 +0530

    Update ExpenseItem.js
    added new components

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,19 +1,12 @@
+import ExpenseDate from './ExpenseDate.js';
+import ExpenseDetails from './ExpenseDetails.js';
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const  expenseDate = new Date(2022 , 11 , 2);
-    const expenseTitle = 'Food';
-    const expenseAmount = 10;
-    const locationOfExpense = 'Mumbai';
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__description">{locationOfExpense}</div>
-        <div className="expense-item__price">rs {expenseAmount}</div>
-        
-      </div>
+      <ExpenseDate date = {props.date} />
+      <ExpenseDetails title = {props.title} amount={props.amount}/>
     </div>
   ); 
 }
