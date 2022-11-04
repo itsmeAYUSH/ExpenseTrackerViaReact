ad5938388f560056255b378face896449dc3189e
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-04 11:47:28 +0530

    Add files via upload

--- components/ExpenseDate.js
+++ components/ExpenseDate.js
@@ -1,4 +1,5 @@
 import './ExpenseDate.css';
+
 function ExpenseDate(props){
     const month = props.date.toLocaleString('en-US', {month : 'long'});
     const day =  props.date.toLocaleString('en-US', {day : '2-digit'});
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import ExpenseDate from './ExpenseDate.js';
 import ExpenseDetails from './ExpenseDetails.js';
+import Card from './Card.js';
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <ExpenseDetails title = {props.title} amount={props.amount}/>
-    </div>
+    </Card>
   ); 
 }
 export default ExpenseItem;
