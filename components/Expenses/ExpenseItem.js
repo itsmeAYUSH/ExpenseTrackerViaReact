5085d7d332cac30da7511d4b3a1f74923fc0bfcd
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-06 18:04:03 +0530

    Add files via upload

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -4,47 +4,53 @@ import Card from '../UI/Card.js';
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("helloo");
+  }
+  const parentNode = document.getElementsByClassName('expenses')[0];
+  const childNode = document.getElementsByClassName('expense-item')[0];
+  const deleteExpense = () =>{
+      console.log('delete')
+      // console.log(parentNode);
+      console.log(childNode); 
+      if(childNode){
+        parentNode.removeChild(childNode);
+        console.log('childnode is present')
+      }
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <ExpenseDetails title = {props.title} amount={props.amount}/>
+      <button onClick = {clickHandler} >Change title</button>
+      <button onClick = {deleteExpense} >Delete expense</button>
     </Card>
   ); 
 }
 export default ExpenseItem;
 
 
-//////////////react///////////////
 
+
+
+
+////////////////////////////////
 // import ExpenseDate from './ExpenseDate.js';
 // import ExpenseDetails from './ExpenseDetails.js';
-// import Card from './Card.js';
+// import Card from '../UI/Card.js';
 // import "./ExpenseItem.css";
-// import React from 'react';
 
-// /*return (
-// <div className='expense-item'>
-//   <ExpenseDate date ={props.date} />
-// <div>
-// <h1>
-//   Expense 1
-// </h1>
-// <h1>
-//   Expense 1
-// </h1>
-// </div>
-// <div>
-// )
-// */
-// function ExpenseItem(props) {
-  
-//   // return React.createElement(
-//   //   'div',
-//   //   {},
-//   //   React.createElement(ExpenseDate, {date:props.date}),
-//   //   React.createElement('h1',{},"Expense 1"),
-//   //   React.createElement('h1',{},"Expense 1"),
-//   // )
-
+// const ExpenseItem = (props) => {
+//   const clickHandler = () => {
+//     console.log("helllo");
+//   }
+//   return (
+//     <Card className="expense-item">
+//       <ExpenseDate date = {props.date} />
+//       <ExpenseDetails title = {props.title} amount={props.amount}/>
+//       <button onClick = {clickHandler} >Change title</button>
+    
+//     </Card>
+//   ); 
 // }
 // export default ExpenseItem;
