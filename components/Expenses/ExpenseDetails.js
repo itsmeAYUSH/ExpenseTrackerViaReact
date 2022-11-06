f14a58f93a5c1112789e928673330f6b747e6a31
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-06 23:40:25 +0530

    Update ExpenseDetails.js

--- components/Expenses/ExpenseDetails.js
+++ components/Expenses/ExpenseDetails.js
@@ -6,7 +6,7 @@ const ExpenseDetails = (props) => {
   return (
     <div className="expense-item__description">
       <h2>{title}</h2>
-      <div className="expense-item__price">rs {amount}</div>
+      <div className="expense-item__price">{amount}</div>
     </div>
   );
 }
