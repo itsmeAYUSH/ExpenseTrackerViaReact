e64a1421c50568e788410e406299349e6d8c7ac0
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-08 23:54:32 +0530

    Update NewExpense.js

--- components/newExpenses/NewExpense.js
+++ components/newExpenses/NewExpense.js
@@ -1,10 +1,18 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+        ...enteredExpenseData,
+        id:Math.random().toString()    
+        };
+        // console.log(expenseData)
+        props.onAddExpense(expenseData);
+      }
   return (
     <div className="new-expense" id="expid">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
     </div>
   );
 };
